c3ec68a1beada18d9f73748abf8eb7db3c29a6e6
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2021-02-21 20:00:49 +0100

    Fixed animation bug on player + added chest animation

---
+++ Assets/_Script/Chest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    UIHandler ui;
+    public enum ChestRarity {common, rare, legendary }
+    bool playerInRange;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (playerInRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.Play("Open");
+                ui.UpdateSubtitle("");
+                Destroy(this);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (ui == null)
+            {
+                ui = FindObjectOfType<UIHandler>();
+            }
+            playerInRange = true;
+            ui.UpdateSubtitle("Press E to open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            ui.UpdateSubtitle("");
+        }
+    }
+}
---
+++ Assets/_Script/UIHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+    public Text subtitles;
+
+    public void UpdateSubtitle(string newText)
+    {
+        subtitles.text = newText;
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+}
