1983570e1b3b74d70ac9d236e0707b9a24693d33
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2021-03-10 11:37:20 +0100

    finished enemy

--- Assets/_Script/Enemy.cs
+++ Assets/_Script/Enemy.cs
@@ -9,12 +9,15 @@ public class Enemy : Actor
     public Transform hpBarCanvas;
     public bool active;
 
+    private Animator anim;
     private Transform target;
     private PlayerBehaviour player;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("Idle", true);
         SetupHealth();
     }
 
@@ -26,36 +29,68 @@ public class Enemy : Actor
     
     void Update()
     {
-        if (target != null)
+        if (dead == false)
         {
-            if (active == false)
+            if (target != null)
             {
-                if (Vector3.Distance(transform.position, player.transform.position) < 5)
+                if (active == false)
                 {
-                    active = true;
+                    if (Vector3.Distance(transform.position, player.transform.position) < 10)
+                    {
+                        active = true;
+                        anim.SetBool("Idle", false);
+                    }
                 }
+                if (active)
+                {
+                    if (Vector3.Distance(transform.position, player.transform.position) > 20)
+                    {
+                        active = false;
+                        anim.SetBool("Idle", true);
+                    }
+                    if (Vector3.Distance(transform.position, player.transform.position) < 1.5f)
+                    {
+                        anim.SetBool("Walking", false);
+                        agent.SetDestination(transform.position);
+                    }
+                    else
+                    {
+                        anim.SetBool("Walking", true);
+                        FoundPlayer();
+                    }
+                }
+                hpBarCanvas.LookAt(target);
             }
-            if (active)
-            {
-                if (Vector3.Distance(transform.position, player.transform.position) > 20)
-                {
-                    active = false;
-                }
-                if (Vector3.Distance(transform.position, player.transform.position) < 1.5f)
-                {
-                    agent.SetDestination(transform.position);
-                }
-                else
-                {
-                    FoundPlayer();
-                }
-            }
-            hpBarCanvas.LookAt(target);
         }
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        if (dead == false)
+        {
+            anim.SetTrigger("GetHit");
+        }
+        base.TakeDamage(damage);
+    }
+
+    public override void OnDeath()
+    {
+        agent.enabled = false;
+        GetComponent<CharacterController>().enabled = false;
+        hpBarCanvas.gameObject.SetActive(false);
+        anim.SetTrigger("Death");
     }
 
     void FoundPlayer()
     {
         agent.SetDestination(player.transform.position);
+    }
+
+    public void Attack()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 1.5f)
+        {
+            player.TakeDamage(25);
+        }
     }
 }
